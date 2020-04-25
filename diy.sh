c819699ed98550f390d90ca2ff8b9911b3d9f447
co1q84 <zeno.gzl@gmail.com>
2020-04-25 10:51:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,8 +11,8 @@ sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_genera
 # rm -rf package/lean/luci-theme-argon
 # 添加新的包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/kenzok8/small.git package/lean/small
-git clone https://github.com/kenzok8/openwrt-packages.git package/lean/others
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/kenzok8/openwrt-packages.git package/others
 #git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 #git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
 #git clone https://github.com/co1q84/luci-theme-netgear.git package/lean/luci-theme-netgear
